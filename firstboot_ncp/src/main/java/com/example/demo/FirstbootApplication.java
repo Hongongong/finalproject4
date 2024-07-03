6d06c70ef563c18bc61b0073c2f9f7ccfb3c1142
Juhee <hongjh2004@gmail.com>
2024-07-03 10:50:42 +0900

    Upload 경로 설정 추가

--- firstboot_ncp/src/main/java/com/example/demo/FirstbootApplication.java
+++ firstboot_ncp/src/main/java/com/example/demo/FirstbootApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import spring.mybatis.board.BoardController;
 
 // - 스프링 부트 시작하는 메인 클래스
 // 1. 내장 tomcat 시작(8080), component-scan 자동 동작 설정
@@ -13,11 +12,13 @@ import spring.mybatis.board.BoardController;
 // 3. src/main/webapp/resources/application.properties 파일에 
 //    /WEB-INF/views/*.jsp 경로와 확장자 설정 필요
 
-@SpringBootApplication
+@SpringBootApplication 
+// 실행 (내장 maven build - run as - maven build... - xxx xxxxx-> 컴파일, 라이브러리 관리, 압축(*.war - pom.xml)
+// / 내장 tomcat -실행)
 //<context:component-scan base-packages> 대체
 // basePackages 생략 시 현재 파일 속해있는 패키지로 인식
 @ComponentScan // (basePackages = "com.example.demo")
-// @ComponentScan(basePackages = "upload")
+@ComponentScan(basePackages = "upload")
 // @ComponentScan(basePackageClasses = BoardController.class) // BoardController 클래스가 위치한 패키지 scan
 
 // 배열 형태로 나열해도됨
--- firstboot_ncp/src/main/java/upload/MyPathConfig.java
+++ firstboot_ncp/src/main/java/upload/MyPathConfig.java
@@ -10,7 +10,10 @@ public class MyPathConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		// url -- 폴더 매핑 저장소
-		registry.addResourceHandler("/upload/**").addResourceLocations("file:///c:/fullstack/upload/");
+		registry.addResourceHandler("/upload/**")
+		.addResourceLocations(UploadInform.uploadLocation);  
+		// .addResourceLocations("file:///usr/mydir/upload/");  
+		// 리눅스에선 /usr/mydir/upload 로 변경 필요
 		
 	}
 	
